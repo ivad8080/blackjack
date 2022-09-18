345cf10a927d45252d91eb1d49d4848cba472a64
Davi SAS <ivad8080@gmail.com>
2022-09-18 17:37:01 -0300

    fix

--- script.js
+++ script.js
@@ -1,1 +1,111 @@
-let player={name:"fichas",chips:30},cards=[],sum=0,hasBlackJack=!1,isAlive=!1,message="",messageEl=document.getElementById("message-el"),sumEl=document.getElementById("sum-el"),cardsEl=document.getElementById("cards-el"),buttonsEl=document.querySelector(".buttons"),playerEl=document.getElementById("player-el");function getRandomCard(){let t=Math.floor(13*Math.random())+1;if(t>10)return 10;if(1===t){let t=0;return t=0===Math.floor(2*Math.random())?1:11,t}return t}playerEl.textContent=`${player.name}: $${player.chips}`;let randomAce=Math.floor(2*Math.random());function getRandomCongrats(){return["AE! VOCÊ FEZ 21! 🥳","ISSO! 21! 🤑","CONSEGUIU! 21! 🤪"][Math.floor(3*Math.random())]}function getRandomYouLose(){return["hihi! Perdeu! 😠","kkk! Se Fudeu! 🤬","haha! Tomou no xx! 🤡"][Math.floor(3*Math.random())]}function startGame(){firstCard=getRandomCard(),cards=[firstCard],sum=firstCard,hasBlackJack=!1,isAlive=!0,renderGame()}function renderGame(){cardsEl.textContent="Cartas: ";for(let t=0;t<cards.length;t++)cardsEl.textContent+=`${cards[t]} `;sumEl.textContent="Total: "+sum,buttonsEl.innerHTML='<button class="disabled">COMEÇAR JOGO</button>',buttonsEl.innerHTML+='<button onclick="newCard()">PEGAR CARTA</button>',sum<=20?(message="Pegue uma carta! 🤔",isAlive=!0,hasBlackJack=!1,buttonsEl.innerHTML='<button class="disabled">COMEÇAR JOGO</button>',buttonsEl.innerHTML+='<button onclick="newCard()">PEGAR CARTA</button>'):21===sum?(message=getRandomCongrats(),isAlive=!1,hasBlackJack=!0,player.chips+=10,playerEl.textContent=player.name+": $"+player.chips,buttonsEl.innerHTML='<button onclick="startGame()">COMEÇAR JOGO</button>',buttonsEl.innerHTML+='<button class="disabled">PEGAR CARTA</button>'):(message=getRandomYouLose(),isAlive=!1,hasBlackJack=!1,player.chips-=1,playerEl.textContent=player.name+": $"+player.chips,buttonsEl.innerHTML='<button onclick="startGame()">COMEÇAR JOGO</button>',buttonsEl.innerHTML+='<button class="disabled">PEGAR CARTA</button>'),messageEl.textContent=message}function newCard(){if(!0===isAlive&&!1===hasBlackJack){let t=getRandomCard();sum+=t,cards.push(t),renderGame()}}console.log(randomAce);
+let player = {
+  name: 'fichas',
+  chips: 30,
+};
+
+let cards = [];
+let sum = 0;
+let hasBlackJack = false;
+let isAlive = false;
+let message = '';
+let messageEl = document.getElementById('message-el');
+let sumEl = document.getElementById('sum-el');
+let cardsEl = document.getElementById('cards-el');
+let buttonsEl = document.querySelector('.buttons');
+let playerEl = document.getElementById('player-el');
+
+playerEl.textContent = `${player.name}: $${player.chips}`;
+
+function getRandomCard() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    let getRandom = Math.floor(Math.random() * 2);
+    let getAceNumber = 0;
+    if (getRandom === 0) {
+      getAceNumber = 1;
+    } else {
+      getAceNumber = 11;
+    }
+    return getAceNumber;
+  } else {
+    return randomNumber;
+  }
+}
+
+let randomAce = Math.floor(Math.random() * 2);
+
+function getRandomCongrats() {
+  let messages = [
+    'AE! VOCÊ FEZ 21! 🥳',
+    'ISSO! 21! 🤑',
+    'CONSEGUIU! 21! 🤪'
+  ];
+  let randomMessages = messages[Math.floor(Math.random() * 3)];
+  return randomMessages; 
+}
+
+function getRandomYouLose() {
+  let messages = [
+    'hihi! Perdeu! 😠',
+    'kkk! Se Fudeu! 🤬',
+    'haha! Tomou no xx! 🤡'
+  ];
+  let randomMessages = messages[Math.floor(Math.random() * 3)];
+  return randomMessages;
+}
+
+function startGame() {
+  firstCard = getRandomCard();
+  cards = [firstCard];
+  sum = firstCard;
+  hasBlackJack = false;
+  isAlive = true;
+  renderGame();
+}
+
+function renderGame() {
+  cardsEl.textContent = 'Cartas: '
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += `${cards[i]} `;
+  }
+  sumEl.textContent = 'Total: ' + sum;
+  buttonsEl.innerHTML = '<button class="disabled">COMEÇAR JOGO</button>';
+  buttonsEl.innerHTML += '<button onclick="newCard()">PEGAR CARTA</button>';
+  if (sum <= 20) {
+    message = 'Pegue uma carta! 🤔';
+    isAlive = true;
+    hasBlackJack = false;
+    buttonsEl.innerHTML = '<button class="disabled">COMEÇAR JOGO</button>';
+    buttonsEl.innerHTML += '<button onclick="newCard()">PEGAR CARTA</button>';
+  } else if (sum === 21) {
+    message = getRandomCongrats();
+    isAlive = false;
+    hasBlackJack = true;
+    player.chips += 10;
+    playerEl.textContent = player.name + ': $' + player.chips;
+    buttonsEl.innerHTML = '<button onclick="startGame()">COMEÇAR JOGO</button>';
+    buttonsEl.innerHTML += '<button class="disabled">PEGAR CARTA</button>';
+  } else {
+    message = getRandomYouLose();
+    isAlive = false;
+    hasBlackJack = false;
+    player.chips -= 1;
+    playerEl.textContent = player.name + ': $' + player.chips;
+    buttonsEl.innerHTML = '<button onclick="startGame()">COMEÇAR JOGO</button>';
+    buttonsEl.innerHTML += '<button class="disabled">PEGAR CARTA</button>';
+  }
+  messageEl.textContent = message;
+}
+
+function newCard() {
+  if (isAlive === true && hasBlackJack === false) {
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    renderGame();
+  }
+}
+
+// console.log(randomAce);
